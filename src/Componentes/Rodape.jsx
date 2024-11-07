bb216d226ed31c61cd507fc30e0493f83c4d4b75
BRUNA AMARO <140859773+BRUNAAM@users.noreply.github.com>
2024-11-07 17:00:09 -0300

    1

--- src/Componentes/Rodape.jsx
+++ src/Componentes/Rodape.jsx
@@ -6,10 +6,10 @@ function Rodape() {
             <div className="redes-sociais">
                 <h2>Siga nossas redes socias, e fique por dentro de todos conteúdos</h2>
                 <a href="https://www.instagram.com/artifys_identidade_visual_mkt" target="_blank" rel="noopener noreferrer">
-                    <img src="src/assets/Instagram.png" alt="Instagram" />
+                    <img src="https://artifys-landing-page.vercel.app/assets/Instagram.png" alt="Instagram" />
                 </a>
                 <a href="https://www.facebook.com/profile.php?id=61566484996663" target="_blank" rel="noopener noreferrer">
-                    <img src="src/assets/facebook.png" alt="Facebook" />
+                    <img src="https://artifys-landing-page.vercel.app/assets/facebook.png" alt="Facebook" />
                 </a>
                 <br /><br /><br />
                 <p>Desenvolvido por [Bruna Amaro da Silva]</p>
